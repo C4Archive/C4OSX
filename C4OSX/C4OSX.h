d0bb6a7323237f5f332514907627e50d1350818d
travis <travis@c4ios.com>
2015-07-28 16:08:38 -0700

    Initial Commit. Creates OSX Framework.

---
+++ C4OSX/C4OSX.h
@@ -0,0 +1,19 @@
+//
+//  C4OSX.h
+//  C4OSX
+//
+//  Created by travis on 2015-07-28.
+//  Copyright © 2015 C4. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for C4OSX.
+FOUNDATION_EXPORT double C4OSXVersionNumber;
+
+//! Project version string for C4OSX.
+FOUNDATION_EXPORT const unsigned char C4OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <C4OSX/PublicHeader.h>
+
+
